d29532ea3d35642e2517d16c8ca6d6fcfc0270d2
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-23 21:30:59 +0100

    Falling blocks: When a block is dropped - it moves down one row per tick

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,21 +2,27 @@ export class Board {
   width;
   height;
   shape;
+  ticks;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.ticks = 0;
   }
 
   drop(shape) {
     this.shape = shape;
   }
 
+  tick() {
+    this.ticks++;
+  }
+
   toString() {
     let board = "";
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        board += this.shape && row === 0 && col === 1 ? this.shape : ".";
+        board += this.shape && row === this.ticks && col === 1 ? this.shape : ".";
       }
       board += "\n";
     }
