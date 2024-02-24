ba5a05e55dcbea2ecd3f2688e30953a55aab45f7
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 10:23:36 +0100

    Falling blocks: When a block reaches the bottom - it stops when it hits the bottom

--- src/Board.mjs
+++ src/Board.mjs
@@ -4,11 +4,16 @@ export class Board {
   width;
   height;
   shape;
+  cells;
   ticks = 0;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.cells = new Array(height).fill(EMPTY_CELL);
+    for (let row = 0; row < height; row++) {
+      this.cells[row] = new Array(width).fill(EMPTY_CELL);
+    }
   }
 
   drop(shape) {
@@ -19,6 +24,10 @@ export class Board {
   }
 
   tick() {
+    if (this.ticks === this.height - 1) {
+      this.cells[this.ticks][1] = this.shape;
+      this.shape = null;
+    }
     this.ticks++;
   }
 
@@ -30,7 +39,7 @@ export class Board {
     let board = "";
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        board += this.hasFalling() && row === this.ticks && col === 1 ? this.shape : EMPTY_CELL;
+        board += this.hasFalling() && row === this.ticks && col === 1 ? this.shape : this.cells[row][col];
       }
       board += "\n";
     }
