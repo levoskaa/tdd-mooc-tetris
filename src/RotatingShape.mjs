1de6e011d34eb31a6c208f4738d81bc4ac67c5d8
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 18:08:49 +0100

    style: format code

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,53 +1,52 @@
 export class RotatingShape {
-    #cells;
-  
-    constructor(cells) {
-      this.#cells = cells;
+  #cells;
+
+  constructor(cells) {
+    this.#cells = cells;
+  }
+
+  static fromString(string) {
+    const rows = string.replaceAll(" ", "").split("\n");
+    const cells = [];
+    for (const row of rows) {
+      cells.push(row.split(""));
     }
-  
-    static fromString(string) {
-      const rows = string.replaceAll(" ", "").split("\n");
-      const cells = [];
-      for (const row of rows) {
-        cells.push(row.split(""));
+    return new RotatingShape(cells);
+  }
+
+  rotateRight() {
+    return this.#rotate("right");
+  }
+
+  rotateLeft() {
+    return this.#rotate("left");
+  }
+
+  #rotate(direction) {
+    const rotatedCells = [];
+    for (let row = 0; row < 3; row++) {
+      rotatedCells[row] = [];
+      for (let col = 0; col < 3; col++) {
+        const rotatedCell = this.#rotateCell(row, col, direction);
+        rotatedCells[row][col] = this.#cells[rotatedCell.row][rotatedCell.col];
       }
-      return new RotatingShape(cells);
     }
+    return new RotatingShape(rotatedCells);
+  }
 
-    rotateRight() {
-        return this.#rotate("right");
-    }
-
-    rotateLeft() {
-        return this.#rotate("left");
-    }
-
-    #rotate(direction) {
-        const rotatedCells = [];
-        for (let row = 0; row < 3; row++) {
-            rotatedCells[row] = [];
-            for (let col = 0; col < 3; col++) {
-                const rotatedCell = this.#rotateCell(row, col, direction);
-                rotatedCells[row][col] = this.#cells[rotatedCell.row][rotatedCell.col];
-            }
-        }
-        return new RotatingShape(rotatedCells);
-    }
-
-    #rotateCell(row, col, direction) {
-        if (direction === "right") {
-            return { row: 2 - col, col: row };
-        } else {
-            return { row: col, col: 2 - row };
-        }
-    }
-  
-    toString() {
-      const shape = [];
-      for (const row of this.#cells) {
-          shape.push(...row, "\n");
-      }
-      return shape.join("");
+  #rotateCell(row, col, direction) {
+    if (direction === "right") {
+      return { row: 2 - col, col: row };
+    } else {
+      return { row: col, col: 2 - row };
     }
   }
-  
+
+  toString() {
+    const shape = [];
+    for (const row of this.#cells) {
+      shape.push(...row, "\n");
+    }
+    return shape.join("");
+  }
+}
