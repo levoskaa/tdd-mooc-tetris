f4fc423b791cdc8f0f2f1baaa45d9de16557453c
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 13:15:23 +0100

    Rotating 3x3 shape: can be rotated right/clockwise

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -13,6 +13,15 @@ export class RotatingShape {
       }
       return new RotatingShape(cells);
     }
+
+    rotateRight() {
+        const rotatedCells = [
+            [this.#cells[2][0], this.#cells[1][0], this.#cells[0][0]],
+            [this.#cells[2][1], this.#cells[1][1], this.#cells[0][1]],
+            [this.#cells[2][2], this.#cells[1][2], this.#cells[0][2]],
+        ];
+        return new RotatingShape(rotatedCells);
+    }
   
     toString() {
       const shape = [];
