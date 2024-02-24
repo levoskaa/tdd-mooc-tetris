ae70cc552d4cf7fe16324caa63ddc80d50949a05
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 11:20:54 +0100

    Rotating 3x3 shape: initial orientation

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,11 @@
+export class RotatingShape {
+  static fromString() {
+    return new RotatingShape();
+  }
+
+  toString() {
+    return "ABC\n" +
+           "DEF\n" +
+           "GHI\n";
+  }
+}
