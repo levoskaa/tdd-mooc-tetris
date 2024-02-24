bd2efdc7a30d4c52061755469ec9ac42862b6f7f
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 12:32:02 +0100

    refactor: extract field for cells

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,11 +1,27 @@
 export class RotatingShape {
-  static fromString() {
-    return new RotatingShape();
+    #cells;
+  
+    constructor() {
+      this.#cells = [
+          ["A", "B", "C"],
+          ["D", "E", "F"],
+          ["G", "H", "I"],
+      ];
+    }
+  
+    static fromString() {
+      return new RotatingShape();
+    }
+  
+    toString() {
+      const shape = [];
+      for (let row = 0; row < this.#cells.length; row++) {
+          for (let col = 0; col < this.#cells[0].length; col++) {
+              shape.push(this.#cells[row][col]);
+          }
+          shape.push("\n");
+      }
+      return shape.join("");
+    }
   }
-
-  toString() {
-    return "ABC\n" +
-           "DEF\n" +
-           "GHI\n";
-  }
-}
+  
