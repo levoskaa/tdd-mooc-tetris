a0279ff9d792d6b66983ba26b1f3f108b073c230
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-24 13:07:48 +0100

    refactor: use for-each in toString

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -16,11 +16,8 @@ export class RotatingShape {
   
     toString() {
       const shape = [];
-      for (let row = 0; row < this.#cells.length; row++) {
-          for (let col = 0; col < this.#cells[0].length; col++) {
-              shape.push(this.#cells[row][col]);
-          }
-          shape.push("\n");
+      for (const row of this.#cells) {
+          shape.push(...row, "\n");
       }
       return shape.join("");
     }
