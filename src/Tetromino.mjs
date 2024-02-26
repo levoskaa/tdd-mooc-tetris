b9d8e15474c6fcebd9cc063c5b1c99ae3ccd287e
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-26 21:04:05 +0100

    refactored

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,6 +1,12 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
+  #shape;
+
+  constructor(shape) {
+    this.#shape = shape;
+  }
+
   static get T_SHAPE() {
     return RotatingShape.fromString(
       `.T.
