6a426402c84159a6dab1afc6ca498a28fe1cdfac
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-03-02 13:09:39 +0100

    refactored

--- src/Board.mjs
+++ src/Board.mjs
@@ -43,17 +43,6 @@ export class Board {
 
   #reachedBottom() {
     return this.#shape.anyFilledCell((row, _) => this.#shapeY + row === this.#height - 1);
-    for (let row = this.#shape.height - 1; row >= 0; row--) {
-      for (let col = 0; col < this.#shape.width; col++) {
-        if (this.#shape.cellAt(row, col) === EMPTY_CELL) {
-          continue;
-        }
-        if (this.#shapeY + row === this.#height - 1) {
-          return true;
-        }
-      }
-    }
-    return false;
   }
 
   #collidedWithBlock() {
